7fe3ae917f3dc209f5fb68b95c5a9682cdeff21e
surajinlab <suraj.in.lab@gmail.com>
2026-05-09 23:52:36 +0530

    first commit

---
+++ src/main/java/com/coder/usersecurity/UsersecurityApplication.java
@@ -0,0 +1,15 @@
+package com.coder.usersecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsersecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsersecurityApplication.class, args);
+		System.out.println("---UserSecurity Application Started---");
+
+	}
+
+}
