63e72b87a19b005b1da9e7493dde7a43f4af94bb
surajinlab <suraj.in.lab@gmail.com>
2026-05-11 00:18:24 +0530

    adding config class

--- src/main/java/com/coder/usersecurity/config/SpringSecurity.java
+++ src/main/java/com/coder/usersecurity/config/SpringSecurity.java
@@ -1,4 +1,10 @@
 package com.coder.usersecurity.config;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSecurity
 public class SpringSecurity {
+
 }
